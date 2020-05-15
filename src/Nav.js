f48394c0d5ff1683c40a71337cb36fa57c7cf4c5
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-05-15 04:34:15 -0400

    center nav items

--- src/Nav.js
+++ src/Nav.js
@@ -5,12 +5,11 @@ export default class Nav extends React.Component {
         return (
             <div >
                 <nav className="navbar navbar-expand-lg navbar-dark">
-                    <a className="navbar-brand" href="/">Restoration KY</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
+                        <div className="navbar-nav text-center mx-auto">
                             <a className="nav-item nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                             <a className="nav-item nav-link" href="/services">Services</a>
                             <a className="nav-item nav-link" href="/gallery">Gallery</a>
