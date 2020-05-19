e6e53a8cb35eeebbffb182c25d5cb7fd9c0f00a7
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-05-19 17:59:18 -0400

    add nav menu to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 export default class Footer extends React.Component {
-    render(){
-        return(
+    render() {
+        return (
             <div className="footer mt-5">
-                Footer
-            </div>
+                <div className="copyright" align="center">
+                    <p className="p-3 text-muted">
+                        &copy; 2020 - {new Date().getFullYear()} www.restorationsky.com - All Rights Reserved.
+                    </p>
+                        <div className="col-lg-4 col-md-8 col-sm-12">
+                            <div className="navbar text-center">
+                                <a className="nav-item nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                                <a className="nav-item nav-link" href="/services">Services</a>
+                                <a className="nav-item nav-link" href="/gallery">Gallery</a>
+                                <a className="nav-item nav-link" href="/contact">Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         )
     }
 }
