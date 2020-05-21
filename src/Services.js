77474da3100d19fbbe5d6886f4fcf9769ef74db4
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-05-20 23:39:06 -0400

    add service names

--- src/Services.js
+++ src/Services.js
@@ -4,7 +4,25 @@ export default class Services extends React.Component {
     render() {
         return (
             <div>
-                Services
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-5 col-lg-4 mx-auto">
+                            <h2 id="header" className="border-bottom">Landscaping</h2>
+                            <p>asdofjaosidfjaosdjf</p>
+                            <img className="" id="homeimg1" src="https://www.houselogic.com/wp-content/uploads/2019/05/spring-landscaping-ideas-yard.jpg" alt="landscaping"></img>
+                            <h2 id="header" className="border-bottom">Masonry</h2>
+                            <h2 id="header" className="border-bottom">Retaining Walls</h2>
+                            <h2 id="header" className="border-bottom">Pavers</h2>
+                            <h2 id="header" className="border-bottom">Fire Places</h2>
+
+                            <h2 id="header" className="border-bottom">Bobcat Service</h2>
+                            <h2 id="header" className="border-bottom">Patios</h2>
+                            <h2 id="header" className="border-bottom">Pressure Cleaning</h2>
+                            <h2 id="header" className="border-bottom">Weatherproofing</h2>
+                            <h2 id="header" className="border-bottom">Tree Service</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
