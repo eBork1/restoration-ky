ba33e1ba650985787b61eaaa21b098409df061f9
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-05-21 16:02:22 -0400

    add sample text and image to each service

--- src/Services.js
+++ src/Services.js
@@ -5,21 +5,94 @@ export default class Services extends React.Component {
         return (
             <div>
                 <div className="container">
-                    <div className="row">
-                        <div className="col-sm-12 col-md-5 col-lg-4 mx-auto">
+                    <div className="row mt-5 mx-auto" id="revRow">
+                        <div className="col-sm-12 col-md-5 col-lg-4">
+                            <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
+                        </div>
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
                             <h2 id="header" className="border-bottom">Landscaping</h2>
                             <p>asdofjaosidfjaosdjf</p>
-                            <img className="" id="homeimg1" src="https://www.houselogic.com/wp-content/uploads/2019/05/spring-landscaping-ideas-yard.jpg" alt="landscaping"></img>
+                        </div>
+                    </div>
+                    <div className="row mt-5 mx-auto" id="revRow">
+                        <div className="col-sm-12 col-md-5 col-lg-4">
+                            <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
+                        </div>
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
                             <h2 id="header" className="border-bottom">Masonry</h2>
+                            <p>asdofjaosidfjaosdjf</p>
+                        </div>
+                    </div>
+                    <div className="row mt-5 mx-auto" id="revRow">
+                        <div className="col-sm-12 col-md-5 col-lg-4">
+                            <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
+                        </div>
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
                             <h2 id="header" className="border-bottom">Retaining Walls</h2>
+                            <p>asdofjaosidfjaosdjf</p>
+                        </div>
+                    </div>
+                    <div className="row mt-5 mx-auto" id="revRow">
+                        <div className="col-sm-12 col-md-5 col-lg-4">
+                            <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
+                        </div>
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
                             <h2 id="header" className="border-bottom">Pavers</h2>
+                            <p>asdofjaosidfjaosdjf</p>
+                        </div>
+                    </div>
+                    <div className="row mt-5 mx-auto" id="revRow">
+                        <div className="col-sm-12 col-md-5 col-lg-4">
+                            <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
+                        </div>
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
                             <h2 id="header" className="border-bottom">Fire Places</h2>
-
+                            <p>asdofjaosidfjaosdjf</p>
+                        </div>
+                    </div>
+                    <div className="row mt-5 mx-auto" id="revRow">
+                        <div className="col-sm-12 col-md-5 col-lg-4">
+                            <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
+                        </div>
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
                             <h2 id="header" className="border-bottom">Bobcat Service</h2>
+                            <p>asdofjaosidfjaosdjf</p>
+                        </div>
+                    </div>
+                    <div className="row mt-5 mx-auto" id="revRow">
+                        <div className="col-sm-12 col-md-5 col-lg-4">
+                            <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
+                        </div>
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
                             <h2 id="header" className="border-bottom">Patios</h2>
+                            <p>asdofjaosidfjaosdjf</p>
+                        </div>
+                    </div>
+                    <div className="row mt-5 mx-auto" id="revRow">
+                        <div className="col-sm-12 col-md-5 col-lg-4">
+                            <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
+                        </div>
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
                             <h2 id="header" className="border-bottom">Pressure Cleaning</h2>
+                            <p>asdofjaosidfjaosdjf</p>
+                        </div>
+                    </div>
+                    <div className="row mt-5 mx-auto" id="revRow">
+                        <div className="col-sm-12 col-md-5 col-lg-4">
+                            <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
+                        </div>
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
                             <h2 id="header" className="border-bottom">Weatherproofing</h2>
+                            <p>asdofjaosidfjaosdjf</p>
+                        </div>
+                    </div>
+                    <div className="row mt-5 mx-auto" id="revRow">
+                        <div className="col-sm-12 col-md-5 col-lg-4">
+                            <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
+                        </div>
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
                             <h2 id="header" className="border-bottom">Tree Service</h2>
+                            <p>asdofjaosidfjaosdjf</p>
                         </div>
                     </div>
                 </div>
