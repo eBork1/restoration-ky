7b9b20a49c6408cdb51e281d07874c326a68835c
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-05-20 23:35:36 -0400

    add menu links to footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,18 +6,19 @@ export default class Footer extends React.Component {
             <div className="footer mt-5">
                 <div className="copyright" align="center">
                     <p className="p-3 text-muted">
-                        &copy; 2020 - {new Date().getFullYear()} www.restorationsky.com - All Rights Reserved.
+                        &copy; {new Date().getFullYear()} www.restorationsky.com - All Rights Reserved.
                     </p>
-                        <div className="col-lg-4 col-md-8 col-sm-12">
-                            <div className="navbar text-center">
-                                <a className="nav-item nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                                <a className="nav-item nav-link" href="/services">Services</a>
-                                <a className="nav-item nav-link" href="/gallery">Gallery</a>
-                                <a className="nav-item nav-link" href="/contact">Contact</a>
-                            </div>
+                    <div className="col-lg-4 col-md-8 col-sm-12">
+                        <div className="navbar text-center">
+                            <a className="nav-item nav-link text-warning" href="/">Home</a>
+                            <a className="nav-item nav-link text-warning" href="/about">About</a>
+                            <a className="nav-item nav-link text-warning" href="/services">Services</a>
+                            <a className="nav-item nav-link text-warning" href="/gallery">Gallery</a>
+                            <a className="nav-item nav-link text-warning" href="/contact">Contact</a>
                         </div>
                     </div>
                 </div>
+            </div>
         )
     }
 }
