b345446c2bce1005d3ddede2ff78b3bd4b6328f8
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-05-21 16:01:13 -0400

    add formspree component to contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Formspree from './Formspree';
 
 export default class Contact extends React.Component {
     render() {
         return (
             <div>
-                Contact
+
+                <div className="container">
+                    <div className="row mt-5">
+                        <div className="col-12 text-center">
+                            <h1 id="header">Contact</h1>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6 col-sm-12 mx-auto">
+                            <Formspree />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
