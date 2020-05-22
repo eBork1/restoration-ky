93d601377cfaeff889fd327806323067819ac93c
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-05-22 19:09:27 -0400

    add login and logout routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/js/bootstrap.js';
 import 'jquery';
 import 'popper.js';
 import './App.css';
+import fire from './Fire';
 import {
     BrowserRouter as Router,
     Switch,
@@ -17,35 +18,72 @@ import Services from './Services';
 import Gallery from './Gallery';
 import Contact from './Contact';
 import Footer from './Footer';
+import Login from './Login';
+import Logout from './Logout';
 
-function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Nav />
-            <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route exact path="/about">
-                        <About />
-                    </Route>
-                    <Route exact path="/services">
-                        <Services />
-                    </Route>
-                    <Route exact path="/gallery">
-                        <Gallery />
-                    </Route>
-                    <Route exact path="/contact">
-                        <Contact />
-                    </Route>
-                </Switch>
-            </Router>
-            
-            <Footer />
-        </div>
-    );
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            user: {},
+        }
+    }
+
+    authListener() {
+        fire.auth().onAuthStateChanged((user) => {
+            if (user) {
+                console.log(user);
+                this.setState({ user });
+                localStorage.setItem('user', user.uid);
+            } else {
+                console.log("not logged in")
+                this.setState({ user: null });
+                localStorage.removeItem('user');
+            }
+            console.log(this.state);
+        })
+    }
+    componentDidMount(){
+        this.authListener();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <Nav />
+                <Router>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route exact path="/about">
+                            <About />
+                        </Route>
+                        <Route exact path="/services">
+                            <Services />
+                        </Route>
+                        <Route exact path="/gallery">
+                            <Gallery 
+                            adminStatus={this.state.user === null ? null : "true"}
+                            />
+                        </Route>
+                        <Route exact path="/contact">
+                            <Contact />
+                        </Route>
+                        <Route exact path="/login">
+                            <Login />
+                        </Route>
+                        <Route exact path="/logout">
+                            <Logout />
+                        </Route>
+                    </Switch>
+                </Router>
+
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
