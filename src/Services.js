80ad543d94b80d27b10f82720b584cfed2b504e5
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-06-03 15:13:57 -0400

    ...

--- src/Services.js
+++ src/Services.js
@@ -9,7 +9,7 @@ export default class Services extends React.Component {
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <img className="" id="serviceimg" src="/landscape.jpg" alt="landscaping"></img>
                         </div>
-                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0" id="landscaping">
                             <h2 id="header" className="border-bottom">Landscaping</h2>
                             <p>asdofjaosidfjaosdjf</p>
                         </div>
@@ -18,7 +18,7 @@ export default class Services extends React.Component {
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <img className="" id="serviceimg" src="/masonry.jpg" alt="masonry"></img>
                         </div>
-                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0" id="masonry">
                             <h2 id="header" className="border-bottom">Masonry</h2>
                             <p>asdofjaosidfjaosdjf</p>
                         </div>
@@ -27,7 +27,7 @@ export default class Services extends React.Component {
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <img className="" id="serviceimg" src="/retainingwall.jpg" alt="retaining walls"></img>
                         </div>
-                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0" id="retaining">
                             <h2 id="header" className="border-bottom">Retaining Walls</h2>
                             <p>asdofjaosidfjaosdjf</p>
                         </div>
@@ -36,7 +36,7 @@ export default class Services extends React.Component {
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <img className="" id="serviceimg" src="/pavers.jpg" alt="pavers"></img>
                         </div>
-                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0" id="pavers">
                             <h2 id="header" className="border-bottom">Pavers</h2>
                             <p>asdofjaosidfjaosdjf</p>
                         </div>
@@ -45,7 +45,7 @@ export default class Services extends React.Component {
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <img className="" id="serviceimg" src="/fireplace.jpg" alt="fireplaces"></img>
                         </div>
-                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0" id="fireplaces">
                             <h2 id="header" className="border-bottom">Fire Places</h2>
                             <p>asdofjaosidfjaosdjf</p>
                         </div>
@@ -54,7 +54,7 @@ export default class Services extends React.Component {
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <img className="border border-dark" id="serviceimg" src="/bobcat.jpg" alt="bobcat service"></img>
                         </div>
-                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0" id="bobcat">
                             <h2 id="header" className="border-bottom">Bobcat Service</h2>
                             <p>asdofjaosidfjaosdjf</p>
                         </div>
@@ -63,7 +63,7 @@ export default class Services extends React.Component {
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <img className="" id="serviceimg" src="/patio.jpeg" alt="Patios"></img>
                         </div>
-                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0" id="patios">
                             <h2 id="header" className="border-bottom">Patios</h2>
                             <p>asdofjaosidfjaosdjf</p>
                         </div>
@@ -72,7 +72,7 @@ export default class Services extends React.Component {
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <img className="" id="serviceimg" src="/pressurecleaning.jpeg" alt="pressure cleaning"></img>
                         </div>
-                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0" id="pressurecleaning">
                             <h2 id="header" className="border-bottom">Pressure Cleaning</h2>
                             <p>asdofjaosidfjaosdjf</p>
                         </div>
@@ -81,7 +81,7 @@ export default class Services extends React.Component {
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <img className="" id="serviceimg" src="/weatherproofing.jpg" alt="weather proofing"></img>
                         </div>
-                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0" id="weatherproofing">
                             <h2 id="header" className="border-bottom">Weatherproofing</h2>
                             <p>asdofjaosidfjaosdjf</p>
                         </div>
@@ -90,7 +90,7 @@ export default class Services extends React.Component {
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <img className="" id="serviceimg" src="/treeservice.jpg" alt="tree service"></img>
                         </div>
-                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0">
+                        <div className="col-sm-12 col-md-5 col-lg-6 ml-0" id="treeservice">
                             <h2 id="header" className="border-bottom">Tree Service</h2>
                             <p>asdofjaosidfjaosdjf</p>
                         </div>
