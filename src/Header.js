ac88bd9c19cad65269eb06ef9e32d800a5d26735
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-06-04 00:23:34 -0400

    add logo, remove banner

--- src/Header.js
+++ src/Header.js
@@ -4,12 +4,12 @@ import './Header.css';
 export default class Header extends React.Component {
     render() {
         return (
-            <div>
-                <header className="header1">
-                    <div>
-                        <img className="mx-auto p-0" id="banner1" src="/RestorationsKYBanner.png" alt="header banner"></img>
+            <div className="header1 container-fluid">
+                <div className="row">
+                    <div className="col-sm-12 col-md-5 col-lg-3">
+                        <img src="/rkylogo1.png" className="rkylogo ml-4 mt-1"></img>
                     </div>
-                </header>
+                </div>
             </div>
         )
     }
